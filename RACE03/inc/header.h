f4180b9eb12e85bc344a7fc1bf2e62de7945110d
LizaNilova <91499156+LizaNilova@users.noreply.github.com>
2021-09-28 17:35:42 +0300

    Race03
    Matrix code, also known as Matrix digital rain or sometimes green rain, is the computercode related to the Matrix. The falling green code is a way of representing the activityof the virtual reality environment of the Matrix on screen."You get used to it, I don't even see the code. All I see is a blond, brunette, redhead."- Cypher

---
+++ RACE03/inc/header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#endif
---
+++ RACE03/src/main.c
@@ -0,0 +1,12 @@
+#include "../inc/header.h"
+
+int main(int argc, char *argv[]) {
+    initsrc();
+    if (argc >= 2){
+        mx_printerr("usage: ./matrix_rain [-s]\n");
+    } 
+    else if(argc == 1){
+        
+    }
+    return 0;
+}
